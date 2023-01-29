cf594269462a3e366228b4da80cd9256ec8378c9
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-01-29 10:51:17 +0000

    adding functions to make it easier to read

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,12 @@
 $(document).ready(function () {
-    var currentDay = moment().format("dddd Do MMMM YYYY");
+
+    // date and time
+    let currentDay = moment().format("dddd Do MMMM, YYYY, HH:mma");
     $("#currentDay").text(currentDay);
 });
 
 // schedual Layout
-for (var i = 9; i <= 17; i++) {
-    var timeblock = $("<div>").addClass("time-block row");
-    var hour = $("<div>")
-        .addClass("hour col-2")
-        .text(i + ":00");
-    var schedule = $("<input>")
-        .addClass("schedule col-8").attr("id", "input1")
-        .attr("data-time", i);
-    var saveBtn = $("<button>")
-        .addClass("saveBtn col-2")
-        .html("<i class='fas fa-save'></i>");
-
-    timeblock.append(hour, schedule, saveBtn);
-    $(".container").append(timeblock);
-
+function pastPresentOrFuture() {
     // check if the hour is in the past present or future
     if (moment().isBefore(moment().hour(i))) {
         schedule.addClass("future");
@@ -29,17 +17,84 @@ for (var i = 9; i <= 17; i++) {
     }
 }
 
-// Input 
-$("#saveBtn").on("click", function () {
-    localStorage.setItem("input1".val());
-    // display message at top of table sayng item has been saved 
-})
+for (var i = 9; i <= 17; i++) {
+    var timeblock = $("<div>").addClass("time-block row");
+    var hour = $("<div>")
+        .addClass("hour col-2")
+        .text(moment(i, "H").format("HHa"));
+    var schedule = $("<input>")
+        .addClass("scheduleInput col-8").attr("id", "input1")
+        .attr("data-time", i);
+    var saveBtn = $("<button>")
+        .addClass("saveBtn col-2")
+        .html("<i class='fas fa-save'></i>");
 
-if (localStorage.getItem("input1")) {
-    $("#input1").val(localStorage.getItem("input1"));
+    timeblock.append(hour, schedule, saveBtn);
+    $(".container").append(timeblock);
+
+    console.log(hour);
+
+    pastPresentOrFuture();
 }
 
 
+function saveMessage() {
+    // item has been saved message
+    let saveMessage = document.createElement("p");
+    saveMessage.text("Your activity has been saved");
+    saveMessage.classList.add("message", "saved");
+    CSSContainerRule.appendChild(saveMessage);
+
+    setTimeout(function () {
+        saveMessage.style.display = "none";
+    }, 2000);
+}
+
+
+// Save to local storage 
+$("#saveBtn").on("click", function () {
+
+    if (schedule === "") {
+        return;
+    }
+
+    let scheduleString = localStorage.getItem("storedSchedule");
+
+    let storedSchedule;
+
+    if (scheduleString === null) {
+        storedSchedule = [];
+    } else {
+        storedSchedule = JSON.parse(scheduleString);
+    }
+
+    let schedualObject = {
+        time: hour,
+        schedule: scheduleInput
+    };
+
+    storedSchedule.push(schedualObject);
+
+    localStorage.setItem("storedSchedule", JSON.stringify(storedSchedule));
+
+    saveMessage();
+})
+
+
+
+
+
+// use .trim() so it gets rid of spaces and stores as a neat string
+
+// delete after ech day
+    // storedSchedule.value = "";
+
+//////// ISSUES ///////
+
+// after 12pm it 13pm 14pm and so on
+// display a message at top of screen saying item has been saved
+// save items to local storage
+// delete items at end of the day
 
 
 
